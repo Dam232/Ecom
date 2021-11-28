3af84861f281c70c5b27fb32c4f13c39de97bdb1
damidu R <drathnayaka446@gmail.com>
2021-11-28 21:23:31 +0530

    First API Setting over

--- API/Startup.cs
+++ API/Startup.cs
@@ -34,7 +34,13 @@ namespace API
 
             //here are the swager contert added from extentions folder
             services.AddSwaggerDocumentation();
-
+            services.AddCors(opt =>
+                      {
+                          opt.AddPolicy("CorsPolicy", policy =>
+                          {
+                              policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                          });
+                      });
 
             // services.AddSwaggerDocumentation();
 
@@ -59,6 +65,7 @@ namespace API
             app.UseRouting();
             app.UseStaticFiles();
 
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
--- Infrastructure/Data/GenericRepository.cs
+++ Infrastructure/Data/GenericRepository.cs
@@ -37,10 +37,31 @@ namespace Infrastructure.Data
             return await ApplySpecification(spec).ToListAsync();
         }
 
+        public async Task<int> CountAsync(ISpecification<T> spec)
+        {
+            return await ApplySpecification(spec).CountAsync();
+        }
         private IQueryable<T> ApplySpecification(ISpecification<T> spec)
         {
             return SpecificationEvaluator<T>.GetQuery(_context.Set<T>().AsQueryable(), spec);
         }
 
+
+
+        public void Add(T entity)
+        {
+            _context.Set<T>().Add(entity);
+        }
+
+        public void Update(T entity)
+        {
+            _context.Set<T>().Attach(entity);
+            _context.Entry(entity).State = EntityState.Modified;
+        }
+
+        public void Delete(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+        }
     }
 }
