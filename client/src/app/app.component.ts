4d3cdd1e8e13d186346a53448cfa7a66418efd0e
damidu R <drathnayaka446@gmail.com>
2021-11-29 18:04:01 +0530

    2ndangular Setting over

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { IPagination } from './models/pagination';
+import { IProduct } from './models/product';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'Ecom Store';
+  products: IProduct[];
+  // constructor(private basketService: BasketService, private accountService: AccountService) { }
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+
+    this.http.get('https://localhost:5001/api/products?pageSize=50')
+      .subscribe((responce: IPagination) => {
+
+          this.products =  responce.data;
+        console.log(responce);
+
+
+      }, error => {
+        console.log(error);
+      }
+
+
+      );
+
+  }
 }
